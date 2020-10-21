49be7f2aa8d5ff52d0bc1fa153e18cc782618688
HaseoQrow <67093883+HaseoQrow@users.noreply.github.com>
2020-10-21 14:33:22 +0200

    Update index.php
    Ajout d'un texte avec classe et police.

--- index.php
+++ index.php
@@ -16,11 +16,15 @@
             top: 45%;
             font-family: "Open Sans";
         }
+        .HubGit {
+            font-family: "Roboto";
+        }
     </style>
 </head>
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, rem recusandae dicta consequatur sit nesciunt earum fuga numquam, iure corporis sunt voluptatibus harum esse corrupti quos veritatis, vel exercitationem eaque!</p>
     <p>I didn't no what I got into, so now I can go back even if I really wanted to, so what more can I do ? Here at the end it's just me and you...</p>
+    <p class="HubGit">Test ! Github vers Git<p>
 </body>
 </html>
