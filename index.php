99b197a20a8724c1017d823ace20b9473cdfa58f
Haseo Qrow <s_karim@outlook.fr>
2020-10-21 11:50:55 +0200

    On a recree un projet avec un nouvel index

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GitSecond</title>
+    <style>
+        body {
+            background-color: black;
+            color: white;
+        }
+        h1 {
+            position: absolute;
+            font-size: 40px;
+            left: 42%;
+            top: 45%;
+            font-family: "Open Sans";
+        }
+    </style>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, rem recusandae dicta consequatur sit nesciunt earum fuga numquam, iure corporis sunt voluptatibus harum esse corrupti quos veritatis, vel exercitationem eaque!</p>
+    <p>I didn't no what I got into, so now I can go back even if I really wanted to, so what more can I do ? Here at the end it's just me and you...</p>
+</body>
+</html>
